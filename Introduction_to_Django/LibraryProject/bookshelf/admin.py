69b722431f5bfcadf9b52d58d468cf6eecbd73cc
Kiruthims <Kiruthimiss@proton.me>
2025-02-17 23:21:34 +0300

    Completed Introduction to Django tasks: created models, implemented CRUD, and customized admin interface

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# Define a custom admin class for the Book model
+class BookAdmin(admin.ModelAdmin):
+
+    list_display = ('title', 'author', 'publication_year')
+
+    search_fields = ('title', 'author')
+
+    list_filter = ('publication_year',)
+
+# Register the Book model with the custom admin options
+admin.site.register(Book, BookAdmin)
--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
     publication_year = models.IntegerField()
 
     def __str__(self):
