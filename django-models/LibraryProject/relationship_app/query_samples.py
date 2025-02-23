c6a0b18c05bc783c03a18f935510c54266d634d2
Kiruthims <Kiruthimiss@proton.me>
2025-02-23 11:26:41 +0300

    Update query_samples.py with required queries

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,6 +2,7 @@ import os
 import django
 import sys
 
+# Set up the environment by adding the project root to sys.path
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
@@ -10,7 +11,8 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 # Query all books by a specific author
 def get_books_by_author(author_name):
-    books = Book.objects.filter(author__name=author_name)
+    author = Author.objects.get(name=author_name)   
+    books = Book.objects.filter(author=author)       
     return [book.title for book in books]
 
 # List all books in a library
@@ -23,8 +25,8 @@ def get_librarian_for_library(library_name):
     librarian = Librarian.objects.get(library__name=library_name)
     return librarian.name if librarian else "No librarian found"
 
-# Sample function calls
-#if __name__ == "__main__":
- #   print("Books by J.K. Rowling:", get_books_by_author("J.K. Rowling"))
-  #  print("Books in City Library:", get_books_in_library("City Library"))
-   # print("Librarian for Central Library:", get_librarian_for_library("Central Library"))
+#if __name__ == '__main__':
+ #   # Sample function calls
+  #  print("Books by J.K. Rowling:", get_books_by_author("J.K. Rowling"))
+   # print("Books in City Library:", get_books_in_library("City Library"))
+    #print("Librarian for City Library:", get_librarian_for_library("City Library"))
