9475fb5ffb7ea0d01a1aebe8a96a28bf60aeec58
Kiruthims <Kiruthimiss@proton.me>
2025-03-02 12:08:55 +0300

    Added forms to bookshelf

---
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class ExampleForm(forms.Form):
+    
+    example_field = forms.CharField(label="Example Field", max_length=100)
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,4 @@
+from .forms import ExampleForm
 from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib.auth.decorators import permission_required
