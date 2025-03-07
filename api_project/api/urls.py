f7475d5dedf18505bfe94bf4a9e71b47a02b82f4
Kiruthims <Kiruthimiss@proton.me>
2025-03-07 19:19:52 +0300

    Naming corrections on views for BookList

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,6 +2,6 @@ from django .urls import path
 from .views import BookListAPIView
 
 urlpatterns = [
-        path('books/', BookListAPIView.as_view(), name = 'book-list'),
+        path('books/', BookList.as_view(), name = 'book-list'),
 ]
 
