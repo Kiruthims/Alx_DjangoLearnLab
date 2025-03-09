1fe99bfba9366c60767c58a354f1f67565373fa2
Kiruthims <Kiruthimiss@proton.me>
2025-03-09 16:35:53 +0300

    Failed to resolve tokenauth, I will try an older python version

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,5 +1,6 @@
 from django .urls import path, include
 from rest_framework .routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import BookList, BookViewSet
 
 
@@ -9,6 +10,7 @@ router.register(r'books_all', BookViewSet, basename = 'book_all')
 urlpatterns = [
         path('__debug__/', include('debug_toolbar.urls')),
         path('books/', BookList.as_view(), name = 'book-list'),
-        path('', include(router.urls))
+        path('', include(router.urls)),
+        path('api_token_auth/', obtain_auth_token, name = 'api_token_auth'),
 ]
 
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -38,11 +38,26 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'api',
     'debug_toolbar',
     'django_extensions',
     
 ]
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+
+    
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated', 
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
