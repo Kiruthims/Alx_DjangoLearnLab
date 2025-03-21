75beea0824da866cb56f5cabd5538ebe296f08d9
Kiruthims <Kiruthimiss@proton.me>
2025-03-21 18:47:52 +0300

    Configured my db to use PostgreSQL

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -78,8 +78,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'your_username',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',  
+        'PORT': '5432',  # Default ya PostgreSQL 
     }
-}
+    }
+
 
 
 # Password validation
