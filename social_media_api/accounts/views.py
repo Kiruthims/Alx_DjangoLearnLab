987909ff7543b26c5a89b9cb8cd334cc0058c43e
Kiruthims <Kiruthimiss@proton.me>
2025-03-26 02:24:21 +0300

    Used CBV instead of FBV in accounts views again

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -32,27 +32,31 @@ class LoginView(APIView):
 
 class FollowUserView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
-
+    
     def post(self, request, user_id):
+        
+        user_qs = CustomUser.objects.all()
         try:
-            user_to_follow = CustomUser.objects.get(id=user_id)
-        except User.DoesNotExist:
+            user_to_follow = user_qs.get(id=user_id)
+        except CustomUser.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-
+        
         if request.user == user_to_follow:
             return Response({"error": "You cannot follow yourself"}, status=status.HTTP_400_BAD_REQUEST)
-
+        
         request.user.following.add(user_to_follow)
         return Response({"message": f"You are now following {user_to_follow.username}"}, status=status.HTTP_200_OK)
 
 class UnfollowUserView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
-
+    
     def post(self, request, user_id):
+        
+        user_qs = CustomUser.objects.all()
         try:
-            user_to_unfollow = User.objects.get(id=user_id)
-        except User.DoesNotExist:
+            user_to_unfollow = user_qs.get(id=user_id)
+        except CustomUser.DoesNotExist:
             return Response({"error": "User not found"}, status=status.HTTP_404_NOT_FOUND)
-
+        
         request.user.following.remove(user_to_unfollow)
         return Response({"message": f"You have unfollowed {user_to_unfollow.username}"}, status=status.HTTP_200_OK)
