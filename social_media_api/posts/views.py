de9a29a1a7ec67ebbcea3374969b1dcec145db1d
Kiruthims <Kiruthimiss@proton.me>
2025-03-26 21:05:58 +0300

    Applied generics.get_object_or_404(Post, pk=pk

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -40,15 +40,12 @@ class FeedView(generics.ListAPIView):
 
 class LikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
-
+    
     def post(self, request, pk):
-        
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
         if not created:
             return Response({"detail": "You already liked this post."}, status=status.HTTP_400_BAD_REQUEST)
-        
-        
         if post.author != request.user:
             Notification.objects.create(
                 recipient=post.author,
@@ -62,7 +59,7 @@ class UnlikePostView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like = Like.objects.filter(user=request.user, post=post)
         if like.exists():
             like.delete()
